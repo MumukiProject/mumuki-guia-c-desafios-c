3491480bb9854b637a00897bed26895734520475
federico.aloi@gmail.com <federico.aloi@gmail.com>
2016-10-11 18:53:13 -0400

    Mumuki Export on 2016-10-11 18:53:13 -0400

--- 00002_Números pares/test.c
+++ 00002_Números pares/test.c
@@ -1,6 +1,6 @@
 describe ("primerPar") {
-  int* arregloSeisElementos = {21, 3, 4, 5, 8, 20};
-  int* arregloTresElementos = {5, 8, 20};
+  int arregloSeisElementos[6] = {21, 3, 4, 5, 8, 20};
+  int arregloTresElementos[3] = {5, 8, 20};
   
   it ("encuentra el primer número par de un arreglo con 6 elementos") {
     should_int(primerPar(arregloSeisElementos)) be equal to(4);
