90bf730d0acb5a116e37381d3d5c3a6f508060b2
federico.aloi@gmail.com <federico.aloi@gmail.com>
2016-10-11 18:29:30 -0400

    Mumuki Export on 2016-10-11 18:29:30 -0400

--- 00001_Vamos al cine/description.md
+++ 00001_Vamos al cine/description.md
@@ -1,5 +1,7 @@
+Queremos modelar la calculadora de tarifas de un cine que tiene dos precios: la tarifa común, de 50 pesos y la tarifa con descuento, de 30 pesos. Para ello, seguiremos las siguientes reglas:
+
 * Menores de 7 años no abonan.
-* Estudiantes hasta 35 años y jubilados abonan 30 pesos.
-* El resto paga la tarifa común, que cuesta 50 pesos.
+* Estudiantes hasta 35 años abonan tarifa con descuento.
+* Jubilados (o sea, mayores de 65 años) pagan tarifa con descuento.
 
 Escribí la función `precioEntrada(edad, esEstudiante)` que a partir de la edad de una persona y si es estudiante calcule cuánto le costará la entrada.
--- 00001_Vamos al cine/test.c
+++ 00001_Vamos al cine/test.c
@@ -1,17 +1,20 @@
 describe ("precioEntrada") {
+  int tarifaComun = 50;
+  int tarifaConDescuento = 30;
+
   it ("los niños no pagan") {
     should_int(precioEntrada(2, 0)) be equal to(0);
   } end
   
-  it ("adultos mayores pagan 30 pesos") {
-    should_int(precioEntrada(65, 0)) be equal to(30);
+  it ("adultos mayores tienen descuento") {
+    should_int(precioEntrada(65, 0)) be equal to(tarifaConDescuento);
   } end
   
-  it ("estudiantes hasta 35 años pagan 30 pesos") {
-    should_int(precioEntrada(34, 1)) be equal to(30);
+  it ("estudiantes hasta 35 años tienen descuento") {
+    should_int(precioEntrada(34, 1)) be equal to(tarifaConDescuento);
   } end  
   
   it ("estudiantes mayores pagan tarifa común") {
-    should_int(precioEntrada(50, 1)) be equal to(30);
+    should_int(precioEntrada(50, 1)) be equal to(tarifaCmoun);
   } end  
 } end
