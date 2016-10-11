0c5930b385a296d7bde6f84f1312ac4aee5646cf
federico.aloi@gmail.com <federico.aloi@gmail.com>
2016-10-11 18:20:32 -0400

    Mumuki Export on 2016-10-11 18:20:32 -0400

--- 00001_Vamos al cine/description.md
+++ 00001_Vamos al cine/description.md
@@ -1,5 +1,5 @@
-* Menores de 3 años no abonan.
+* Menores de 7 años no abonan.
 * Estudiantes hasta 35 años y jubilados abonan 30 pesos.
-* El resto paga 50 pesos.
+* El resto paga la tarifa común, que cuesta 50 pesos.
 
 Escribí la función `precioEntrada(edad, esEstudiante)` que a partir de la edad de una persona y si es estudiante calcule cuánto le costará la entrada.
--- 00001_Vamos al cine/test.c
+++ 00001_Vamos al cine/test.c
@@ -2,4 +2,16 @@ describe ("precioEntrada") {
   it ("los ninios no pagan") {
     should_int(precioEntrada(2, 0)) be equal to(0);
   } end
+  
+  it ("adultos mayores pagan 30 pesos") {
+    should_int(precioEntrada(65, 0)) be equal to(30);
+  } end
+  
+  it ("estudiantes hasta 35 años pagan 30 pesos") {
+    should_int(precioEntrada(34, 1)) be equal to(30);
+  } end  
+  
+  it ("estudiantes mayores pagan tarifa común") {
+    should_int(precioEntrada(50, 1)) be equal to(30);
+  } end  
 } end
