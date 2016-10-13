c5441f70137faea5e64d5e6669437bbe62a0a2fc
franco@mumuki.org <franco@mumuki.org>
2016-10-13 17:21:56 -0400

    Mumuki Export on 2016-10-13 17:21:56 -0400

--- 00002_Números pares/test.c
+++ 00002_Números pares/test.c
@@ -1,6 +1,11 @@
 describe ("primerPar") {
   int arregloSeisElementos[6] = {21, 3, 4, 5, 8, 20};
   int arregloTresElementos[3] = {5, 8, 20};
+  int arregloCincoElementos[5] = {45, 43, 36, 25, 14};
+
+  it ("el primer par del arreglo {45, 43, 36, 25, 14} es 14") {
+    should_int(primerPar(arregloCincoElementos)) be equal to(14);
+  } end
   
   it ("el primer par del arreglo {21, 3, 4, 5, 8, 20} es 4") {
     should_int(primerPar(arregloSeisElementos)) be equal to(4);
