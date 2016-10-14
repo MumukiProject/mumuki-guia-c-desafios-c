1a41f5ac822a406de39cc319f458f32f4d66bb80
federico.aloi@gmail.com <federico.aloi@gmail.com>
2016-10-14 14:10:04 -0400

    Mumuki Export on 2016-10-14 14:10:04 -0400

---
+++ 00004_El promedio, recargado/test.c
@@ -0,0 +1,16 @@
+describe ("promedioSinAplazos") {
+  it ("el promedio sin aplazos de {8, 4, 2} es 6") {
+    int notas[3] = {8, 4, 2};
+    should_float(promedioConAplazos(notas, 3)) be equal to(6.0);
+  } end
+  
+  it ("el promedio sin aplazos de {10, 7, 8, 6, 3} es 7.75") {
+    int notas[5] = {10, 7, 8, 6, 3};
+    should_float(promedio(notas, 5)) be equal to(7.75);
+  } end
+  
+  it ("el promedio sin aplazos del arreglo vacío es 0") {
+    int notas[0] = {};
+    should_float(promedio(notas, 0)) be equal to(0.0);
+  } end 
+} end
