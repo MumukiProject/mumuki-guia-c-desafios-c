b46e26a9b5c19f7e64d4b9fae9e195bbd71f0935
federico.aloi@gmail.com <federico.aloi@gmail.com>
2016-10-14 11:42:23 -0400

    Mumuki Export on 2016-10-14 11:42:23 -0400

--- 00003_En promedio/test.c
+++ 00003_En promedio/test.c
@@ -1,16 +1,16 @@
 describe ("promedio") {
   it ("el promedio de {4, 2, 3} es 3") {
     int notas[3] = {4, 2, 3};
-    should_float(promedio(notas, 3)) be equal to(3.0);
+    should_float(promedio(notas, 3)) be equal to(3.0f);
   } end
   
   it ("el promedio de {10, 7, 8, 6} es 7.75") {
     int notas[4] = {10, 7, 8, 6};
-    should_float(promedio(notas, 4)) be equal to(7.75);
+    should_float(promedio(notas, 4)) be equal to(7.75f);
   } end
   
   it ("el promedio del arreglo vacío es 0") {
     int notas[0] = {};
-    should_float(promedio(notas, 0)) be equal to(0.0);
+    should_float(promedio(notas, 0)) be equal to(0.0f);
   } end 
 } end
